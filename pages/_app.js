3ab52e42711b5c40c0dd682e2a717b0e7af525d7
Eva Gonzalez <eva.gonzalez@ingka.ikea.com>
2022-09-12 16:13:10 +0200

    feature exploring routes and first pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Learning NextJS</title>
+      <meta name="description" content="Creating an app for learning NextJS" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <header>News-App</header>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ pages/article/[id].js
@@ -0,0 +1,9 @@
+
+
+function Article() {
+  return (
+    <div>Esto es una ruta para Article :D </div>
+  )
+}
+
+export default Article;
